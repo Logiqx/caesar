59bdf9eb9c2a2931cefb79c9a9c35e14f2457c87
Logiqx <software@mikeg.me.uk>
2007-01-04 08:48:26 +0000

    No 2001+ games currently

--- data/games/_check_nonmame.bat
+++ data/games/_check_nonmame.bat
@@ -6,8 +6,8 @@ notepad mamediff.log
 mamediff -v nonmame.cps2.dat "H:\Logiqx\Dats\Recent\Systems\CPS-2\CPS-2 YYYYMMDD (Non-MAME).dat"
 notepad mamediff.log
 
-mamediff -v nonmame.neo.2001+.dat "H:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (2001+).dat"
-notepad mamediff.log
+REM mamediff -v nonmame.neo.2001+.dat "H:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (2001+).dat"
+REM notepad mamediff.log
 
 mamediff -v nonmame.neo.misc.dat "H:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (Misc).dat"
 notepad mamediff.log
