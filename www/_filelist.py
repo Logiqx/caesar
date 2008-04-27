c4dc128f6d7b45cb58c1e8d746dbeae590eda104
Logiqx <software@mikeg.me.uk>
2008-04-27 17:01:18 +0000

    Do not include own filename

--- www/_filelist.py
+++ www/_filelist.py
@@ -13,13 +13,15 @@ ignoreDirs = \
 ]
 
 node = platform.node().lower().split('.')[0]
-f = open('_filelist_' + node + '.txt', "w")
+fn = '_filelist_' + node + '.txt'
+f = open(fn, "w")
 
 for root, dirs, files in os.walk('.', topdown = True):
 
 	for file in files:
-		size = str(os.stat(os.path.join(root, file)).st_size)
-		f.write(root.replace(os.sep, '/') + '\t' + file + '\t' + size + '\n')
+		if file != fn:
+			size = str(os.stat(os.path.join(root, file)).st_size)
+			f.write(root.replace(os.sep, '/') + '\t' + file + '\t' + size + '\n')
 
 	for ignoreDir in ignoreDirs:
 		ignoreDir = ignoreDir.replace('/', os.sep)
