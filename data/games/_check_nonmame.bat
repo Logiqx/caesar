a3f91cb534b0d5fe80df3273de3e956e81cb60c8
Logiqx <software@mikeg.me.uk>
2008-10-22 17:54:35 +0000

    MAME v0.128

--- data/games/_check_nonmame.bat
+++ data/games/_check_nonmame.bat
@@ -1,18 +1,18 @@
 @echo off
 
-mamediff -v nonmame.cps1.dat "H:\Logiqx\Dats\Recent\Systems\CPS-1\CPS-1 YYYYMMDD (Non-MAME).dat"
+mamediff -v nonmame.cps1.dat "E:\Logiqx\Dats\Recent\Systems\CPS-1\CPS-1 YYYYMMDD (Non-MAME).dat"
 notepad mamediff.log
 
-mamediff -v nonmame.cps2.dat "H:\Logiqx\Dats\Recent\Systems\CPS-2\CPS-2 YYYYMMDD (Non-MAME).dat"
+mamediff -v nonmame.cps2.dat "E:\Logiqx\Dats\Recent\Systems\CPS-2\CPS-2 YYYYMMDD (Non-MAME).dat"
 notepad mamediff.log
 
-REM mamediff -v nonmame.neo.2001+.dat "H:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (2001+).dat"
+REM mamediff -v nonmame.neo.2001+.dat "E:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (2001+).dat"
 REM notepad mamediff.log
 
-mamediff -v nonmame.neo.misc.dat "H:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (Misc).dat"
+mamediff -v nonmame.neo.misc.dat "E:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (Misc).dat"
 notepad mamediff.log
 
-mamediff -v nonmame.neo.decrypted.dat "H:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (Decrypted).dat"
+mamediff -v nonmame.neo.decrypted.dat "E:\Logiqx\Dats\Recent\Systems\Neo-Geo\Neo-Geo YYYYMMDD (Decrypted).dat"
 notepad mamediff.log
 
 rm -f mamediff.log mamediff.out
