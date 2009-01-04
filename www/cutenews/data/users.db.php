093d9e897a8268ce3840c858f6df393cf74959ef
Logiqx <software@mikeg.me.uk>
2009-01-04 22:49:35 +0000

    Done the quarterly archiving

--- www/cutenews/data/users.db.php
+++ www/cutenews/data/users.db.php
@@ -1,5 +1,5 @@
 <?PHP die("You don't have access to open this file !!!"); ?>
 1177438307|1|admin|a28f8f4e4f8976fb30d2efe4b04d0ffb||hosting@logiqx.com|1|1||1177611437||
-1177438455|1|logiqx|a28f8f4e4f8976fb30d2efe4b04d0ffb||hosting@logiqx.com|2|1||1216071266||
+1177438455|1|logiqx|a28f8f4e4f8976fb30d2efe4b04d0ffb||hosting@logiqx.com|2|1||1231109130||
 1177438475|2|pi|1ca2c3481e9c59937f6d69d4de73af98||pi@logiqx.com|1|1||1177460700||
 1177439429|2|neoforma|c26181045ca9a219aa578875badb3c16||neoforma@mail.ru|60|1||1211494835||
