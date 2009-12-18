cfa9566f9c13c3186070ec5a05cd097e71c89471
Logiqx <software@mikeg.me.uk>
2009-12-18 06:14:17 +0000

    MAME v0.135 and FBA v0.2.97.07

--- www/upload/_upload.bat
+++ www/upload/_upload.bat
@@ -3,7 +3,7 @@
 ECHO Uploading to %CAESAR_LOGIN%@%CAESAR_FTP%...
 ECHO.
 
-pscp -P 5334 all.zip %CAESAR_LOGIN%@%CAESAR_FTP%:www/caesar/upload/all.zip
+pscp all.zip %CAESAR_LOGIN%@%CAESAR_FTP%:www/caesar/upload/all.zip
 
 rm -f all.zip
 
